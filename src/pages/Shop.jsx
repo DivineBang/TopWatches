c617d88fc885f05baa566d47e78a9be9b83ecb10
Dmytro Didenko <dmitriydidenko02@gmail.com>
2023-10-10 19:00:05 +0300

    mob-all

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -9,55 +9,44 @@ function Shop(){
     const {items,addToCard,search,isItemAdded } = React.useContext(AppContext);
     return(
         <div className="Main">
-
-           
           
 
             <div className="container-xxl watches">
                 
                 <div className="cards__set">
-                    <div className="row">
-                        <div className="col">
-                            <div className="card__set">
-                            {items.filter((obj) => obj.title.toLowerCase().includes(search.toLowerCase())).map((obj,index) =>(
+                    <div className="card__set">
+                    {items.filter((obj) => obj.title.toLowerCase().includes(search.toLowerCase())).map((obj,index) =>(
 
-                                <Card  key={index} addEd={isItemAdded(obj && obj.id)} onCard={(product) => addToCard(product)}  imgCard={obj.img} modelCard={obj.model}  titleCard={obj.title} priceCard={obj.price} />
+                        <Card  key={index} addEd={isItemAdded(obj && obj.id)} onCard={(product) => addToCard(product)}  imgCard={obj.img} modelCard={obj.model}  titleCard={obj.title} priceCard={obj.price} />
 
-                            )) 
-                            }              
-                            </div>
-                        </div>
-                    </div>    
+                    )) 
+                    }              
+                    </div>
+  
                 </div>    
                 <div className="cards__set">
-                    <div className="row">
-                        <div className="col">
-                            <div className="card__set">
-                            {items.filter((obj) => obj.title.toLowerCase().includes(search.toLowerCase())).map((obj,index) =>(
+                    <div className="card__set">
+                    {items.filter((obj) => obj.title.toLowerCase().includes(search.toLowerCase())).map((obj,index) =>(
 
-                                <Card  key={index} addEd={isItemAdded(obj && obj.id)} onCard={(product) => addToCard(product)}  imgCard={obj.img} modelCard={obj.model}  titleCard={obj.title} priceCard={obj.price} />
+                        <Card  key={index} addEd={isItemAdded(obj && obj.id)} onCard={(product) => addToCard(product)}  imgCard={obj.img} modelCard={obj.model}  titleCard={obj.title} priceCard={obj.price} />
 
-                            )) 
-                            }              
-                            </div>
-                        </div>
-                    </div>    
-                </div> 
+                    )) 
+                    }              
+                    </div>
+  
+                </div>  
                 <div className="cards__set">
-                    <div className="row">
-                        <div className="col">
-                            <div className="card__set">
-                            {items.filter((obj) => obj.title.toLowerCase().includes(search.toLowerCase())).map((obj,index) =>(
+                    <div className="card__set">
+                    {items.filter((obj) => obj.title.toLowerCase().includes(search.toLowerCase())).map((obj,index) =>(
 
-                                <Card  key={index} addEd={isItemAdded(obj && obj.id)} onCard={(product) => addToCard(product)}  imgCard={obj.img} modelCard={obj.model}  titleCard={obj.title} priceCard={obj.price} />
+                        <Card  key={index} addEd={isItemAdded(obj && obj.id)} onCard={(product) => addToCard(product)}  imgCard={obj.img} modelCard={obj.model}  titleCard={obj.title} priceCard={obj.price} />
 
-                            )) 
-                            }              
-                            </div>
-                        </div>
-                    </div>    
-                </div>   
-                  
+                    )) 
+                    }              
+                    </div>
+  
+                </div>  
+
             </div>
       </div>
     );
